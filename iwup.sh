c0801a63bd0cc8deec8023160f4ef8d14fa345b1
mwfarb <mwfarb@andrew.cmu.edu>
2024-09-11 20:21:44 +0000

    adding failed query for interface name

--- iwup.sh
+++ iwup.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+
+# testing query for wireless interface
+#interface="wlan0"
+#if /sbin/ethtool wlan0 | grep -q "No such device"; then
+##if ip link show wlan0 | grep -q "does not exist"; then
+#  interface=$(find /sys/class/net/ -name "wlx*" -exec basename \{} \;)
+#  echo "$interface renamed to wlan0"
+#  ip link set dev $interface name wlan0
+#fi
+
 ip link set dev wlx9cefd5f986da name wlan0
 ip link set wlan0 down
 iw wlan0 set type mp

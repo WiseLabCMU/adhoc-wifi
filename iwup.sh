05dcb56beb4662ea06a9be464d5676618c9d3a5a
mwfarb <mwfarb@andrew.cmu.edu>
2024-09-12 22:47:54 -0400

    add ip_part param, stop network manager

--- iwup.sh
+++ iwup.sh
@@ -1,18 +1,29 @@
 #!/bin/bash
+# Usage: sudo ./iwup.sh 3
+
+if [ $# -eq 0 ]
+  then
+    echo "No arguments supplied, add last ip subnet."
+    exit 1
+fi
+ip_part=$1
+
+# remove desktop network manager
+sudo systemctl stop network-manager.service
+#sudo systemctl disable network-manager.service
 
 # testing query for wireless interface
-#interface="wlan0"
-#if /sbin/ethtool wlan0 | grep -q "No such device"; then
-##if ip link show wlan0 | grep -q "does not exist"; then
-#  interface=$(find /sys/class/net/ -name "wlx*" -exec basename \{} \;)
-#  echo "$interface renamed to wlan0"
-#  ip link set dev $interface name wlan0
-#fi
+interface="wlan0"
+if /sbin/ethtool wlan0 | grep -q "No such device"; then
+#if ip link show wlan0 | grep -q "does not exist"; then
+ interface=$(find /sys/class/net/ -name "wlx*" -exec basename \{} \;)
+ echo "$interface renamed to wlan0"
+ sudo ip link set dev $interface name wlan0
+fi
+# sudo ip link set dev wlx9cefd5f986da name wlan0
 
-ip link set dev wlx9cefd5f986da name wlan0
-ip link set wlan0 down
-iw wlan0 set type mp
-ip link set wlan0 up
-iw wlan0 mesh join wiseMesh freq 2422
-ip addr add 192.168.1.3/24 dev wlan0
-
+sudo ip link set wlan0 down
+sudo iw wlan0 set type mp
+sudo ip link set wlan0 up
+sudo iw wlan0 mesh join wiseMesh freq 2422
+sudo ip addr add 192.168.1.$ip_part/24 dev wlan0
